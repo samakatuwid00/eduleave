f4a61f923fd1a37d42b098ddca1e0990a8d6bef5
samakatuwid00 <abaygherjr07@gmail.com>
2026-07-03 21:20:45 +0800

    Update mailing system for now

--- config/mail.php
+++ config/mail.php
@@ -112,5 +112,9 @@ return [
         'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
+    // 'reply_to' => [
+    //     'address' => env('MAIL_REPLY_TO_ADDRESS'),
+    //     'name' => env('MAIL_REPLY_TO_NAME', env('MAIL_FROM_NAME')),
+    // ],
 
 ];
